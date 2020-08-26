910f43ca72d7f6b0fd3619a9f2fb97a789b8e07e
GTSpray <69210426+GTSpray@users.noreply.github.com>
2020-08-26 19:57:21 +0200

    remove localhost

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const loginRouter = require("./routes/login.route");
 const indexRouter = require("./routes/oauth2.route");
 const usersRouter = require("./routes/users.route");
 
+const port = process.env.PORT || 8080
+
 const app = express();
 
 app
+  .set('port', port)
   .use(logger("dev"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
@@ -20,6 +23,6 @@ app
   .use("/login", loginRouter)
   .use("/users", usersRouter);
 
-const listener = app.listen(8080, () => {
-  console.log("Listening on port " + listener.address().port);
+const server = app.listen(app.get('port'), () => {
+  console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
 });
